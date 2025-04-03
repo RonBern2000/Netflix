75f9b828468ab7e3c67f3d67a14ef8231dc84b87
Ron Bernshtein <ronbernshtein277@gmail.com>
2025-04-03 12:32:15 +0300

    Updated library

--- BackendUserService/src/utils/jwt.ts
+++ BackendUserService/src/utils/jwt.ts
@@ -6,7 +6,6 @@ export const sign = (payload: IUserPayload): string => {
     return jwt.sign(payload, process.env.JWT_KEY!, { expiresIn: "1h" })
 }
 
-
 export const verify = (token: string) : IUserPayload | null => {
   if(token.includes(TOKENS.Bearer))
     token = token.substring(TOKENS.Bearer.length + 1);
--- Proxy/middleware/authenticate.ts
+++ Proxy/middleware/authenticate.ts
@@ -2,11 +2,11 @@ import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import { JWT_KEY } from '../src/config/env';
 import { BadRequestError } from '@netflix-utils/shared';
+import { verify } from '@netflix-utils/shared'
 
 export const Authenticate = (req: Request, res: Response, next: NextFunction) => {
     const { path, header } = req;
 
-    //TODO: check that Bearer is inside the token
     if (path.startsWith('/api/v1/movies/popular')){
         return next();
     }
@@ -14,7 +14,7 @@ export const Authenticate = (req: Request, res: Response, next: NextFunction) =>
         const token = header('token');
 
         if (token){
-        const verified = jwt.verify(token, JWT_KEY!); //TODO: move to utils
+        const verified = verify(token, JWT_KEY!);
         if (verified) {
             return next()
         } else {
