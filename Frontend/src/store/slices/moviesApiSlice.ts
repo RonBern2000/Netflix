6453819ed93e577bd49cf2b755a1b43ce2e2a46d
Ron Bernshtein <ronbernshtein277@gmail.com>
2025-04-09 00:25:04 +0300

    Added frontend api call for all movies by genre

--- Frontend/src/store/slices/moviesApiSlice.ts
+++ Frontend/src/store/slices/moviesApiSlice.ts
@@ -12,16 +12,12 @@ export const moviesApiSlice = createApi({
                 query: () => "/movies/api/v1/movies/popular",
                 transformResponse: (response: { popularMovies: IMovie[] }) => response.popularMovies,
             }),
-            getAllMovies: builder.query<IMovie[], void>({
-                query: () => "/movies/api/v1/movies/allMovies",
-                transformResponse: (response: { allMovies: IMovie[] }) => response.allMovies,
-            }),
-            getMovieTrailer: builder.query<string, number>({
-                query: (movieId) => `/movies/api/v1/movies/movieTrailer/${movieId}`,
-                transformResponse: (response: { key: string }) => response.key,
+            getAllMovies: builder.query<Record<string, IMovie[]>, void>({
+                query: () => "/movies/api/v1/movies/allMoviesByGenres",
+                transformResponse: (response: { allMoviesByGenre: Record<string, IMovie[]> }) => response.allMoviesByGenre,
             }),
         };
     }
 });
 
-export const { useGetPopMoviesQuery, useGetAllMoviesQuery, useGetMovieTrailerQuery} = moviesApiSlice;
+export const { useGetPopMoviesQuery, useGetAllMoviesQuery} = moviesApiSlice;
