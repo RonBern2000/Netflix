73b7adafae14a8ee5045890271cf3911535bd303
Ron Bernshtein <ronbernshtein277@gmail.com>
2025-05-03 04:02:04 +0300

    trying with path rewrite

--- Proxy/src/app.ts
+++ Proxy/src/app.ts
@@ -79,6 +79,9 @@ app.use(
     target: MOVIES_URL,
     changeOrigin: true,
     secure: false,
+    pathRewrite: {
+      "^/api/v1/movies": "",
+    },
     on: {
       error: (error, req, res, target) => {
         console.error(error);
