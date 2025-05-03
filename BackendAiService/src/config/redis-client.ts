866021ad8267880db2675b098e5d24cf4b32deca
Ron Bernshtein <ronbernshtein277@gmail.com>
2025-05-03 15:48:24 +0300

    another fix to ai redis

--- BackendAiService/src/config/redis-client.ts
+++ BackendAiService/src/config/redis-client.ts
@@ -1,12 +1,11 @@
 import { createClient } from 'redis';
-import { REDIS_PORT, REDIS_URI } from './env';
 
 const redis = createClient({
-    url: `redis://${REDIS_URI}:${REDIS_PORT}`,
+    url: `redis://${process.env.REDIS_URI}:${process.env.REDIS_PORT}`,
 });
 
 redis.on('connect', () => {
-  console.log(`Connected to Redis on port ${REDIS_PORT}`);
+  console.log(`Connected to Redis on port ${process.env.REDIS_PORT}`);
 });
 
 redis.on('error', (err) => {
@@ -16,7 +15,7 @@ redis.on('error', (err) => {
 const connectRedis = async () => {
   try {
     await redis.connect();
-    console.log(`Redis client connected on port ${REDIS_PORT}`);
+    console.log(`Redis client connected on port ${process.env.REDIS_PORT}`);
   } catch (error) {
     console.error('Error connecting to Redis:', error);
   }
@@ -31,3 +30,31 @@ export default redis;
 //         host: REDIS_URI,
 //         port: Number(REDIS_PORT),
 //     },
+
+// import { createClient } from 'redis';
+// import { REDIS_PORT, REDIS_URI } from './env';
+
+// const redis = createClient({
+//     url: `redis://${process.env.REDIS_URI}:${process.env.REDIS_PORT}`,
+// });
+
+// redis.on('connect', () => {
+//   console.log(`Connected to Redis on port ${REDIS_PORT}`);
+// });
+
+// redis.on('error', (err) => {
+//   console.error('Redis connection error:', err);
+// });
+
+// const connectRedis = async () => {
+//   try {
+//     await redis.connect();
+//     console.log(`Redis client connected on port ${REDIS_PORT}`);
+//   } catch (error) {
+//     console.error('Error connecting to Redis:', error);
+//   }
+// };
+
+// connectRedis();
+
+// export default redis;
