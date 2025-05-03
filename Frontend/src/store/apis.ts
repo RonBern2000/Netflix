71e56237ddbb8f11181d9c93deca082beb652f2b
Ron Bernshtein <ronbernshtein277@gmail.com>
2025-05-03 03:10:21 +0300

    updated the baseUrl in the frontend

--- Frontend/src/store/apis.ts
+++ Frontend/src/store/apis.ts
@@ -11,7 +11,7 @@ const mutex = new Mutex();
 
 // "http://localhost:5000"
 export const baseQuery = fetchBaseQuery({
-    baseUrl: "http://net-flex-prod.site",
+    baseUrl: "https://www.net-flex-prod.site",
     prepareHeaders: (headers, { getState }) => {
         const token = (getState() as RootState).auth.accessToken;
         if(token){
