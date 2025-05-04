eb1d5f2d053c9e6d7047cd63b44a3251e9a7a3c0
Ron Bernshtein <ronbernshtein277@gmail.com>
2025-05-04 17:44:11 +0300

    deleted clg

--- BackendMoviesService/src/services/movies-service.ts
+++ BackendMoviesService/src/services/movies-service.ts
@@ -9,8 +9,6 @@ import { MoviesByGenre } from "../DTOs/genre-movie-dto";
 import { IGenre } from "../interfaces/IGenre";
 import { setMoviesTrailer } from "../utils/setMoviesTrailer";
 import { BadRequestError } from "@netflix-utils/shared";
-//import redis from "../config/redis-client";
-
 @injectable()
 export class MoviesService implements IMoviesService{
     
@@ -72,59 +70,3 @@ export class MoviesService implements IMoviesService{
         return allMoviesByGenres;
     }
 }
-
-    // async getAllMovies(): Promise<IMovie[] | null> {
-    //     const allMovies = await tmdbGetMoviesByGenre();
-    //     console.log(allMovies);
-    //     return allMovies;
-        // await redis.del(TOKENS.allMovies); // only for testing
-        // let allMovies : IMovie[] | null = await this.moviesRepository.getAllMovies();
-        // // console.log('Is there redis:',allMovies);
-        // if(!allMovies){
-        //     allMovies = await tmdbGetAllMovies(5);
-        //     if(allMovies){
-        //         for (const movie of allMovies) {
-        //             const key = await tmdbGetTrailer(movie.id);
-        //             movie.key = key!;
-        //         }
-        //     }
-        //     await this.moviesRepository.setAllMovies(allMovies!);
-        //     const genres = await tmdbGetGenres();
-        //     // await this.moviesRepository.setGenres(genres!);
-        //     // const orderedByGenre: Record<string, IMovie[]> = await orderMoviesByGenre(allMovies!, genres!);
-        //     // Object.entries(orderedByGenre).forEach(async ([genre, movies]) => {
-        //     //     const moviesByGenre: MoviesByGenre = {genre, movies};
-        //     //     await this.moviesRepository.setMoviesByGenre(moviesByGenre);
-        //     // })
-        //     // console.log('Ron the king:', allMovies);
-        // }
-        // console.log(allMovies?.length);
-        // return allMovies;
-//     }
-
-    
-// }
-// async getMovieTrailer(movieId: number): Promise<string | null>{
-//     const key = await tmdbGetTrailer(movieId);
-//     return key;
-// }
-
- // await redis.del(TOKENS.popularMovies);
-        // let data: string | null = await redis.get(TOKENS.popularMovies);
-        // console.log('Is there data:',data);
-        // let popMovies:IMovie[] | null = data ? JSON.parse(data) : null;
-
-
-
-    // async getPopularMovies(): Promise<IMovie[] | null> {
-    //     let data: string | null = await redis.get(TOKENS.popularMovies);
-    //     let popMovies:IMovie[] | null = data ? JSON.parse(data) : null;
-    //     console.log('Movies came from redis:',popMovies);
-    //     if(!popMovies){
-    //         popMovies = await tmdbGetPopular();
-    //         await redis.set(TOKENS.popularMovies, JSON.stringify(popMovies));
-    //         console.log('Movies came from db:', popMovies);
-    //     }
-    //     return popMovies ? popMovies.slice(0, 10) : null; // Top 10
-    // }
-
